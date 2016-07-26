17b74163fc8f00360ed307f22abd37b61f7f3228
Sirsendu Konar <skonar@lexmark.com>
2016-07-27 01:08:16 +0530

    Update NotificationController to add message

--- grails-app/controllers/demoactivemq/NotificationController.groovy
+++ grails-app/controllers/demoactivemq/NotificationController.groovy
@@ -2,5 +2,9 @@ package demoactivemq
 
 class NotificationController {
 
-    def index() { }
+    def index = {
+		def message = "Hi, this is a Hello World with JMS & ActiveMQ, " + new Date()
+		sendJMSMessage("queue.notification", message)
+		render message
+	}
 }
